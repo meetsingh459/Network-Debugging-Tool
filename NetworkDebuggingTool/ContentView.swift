22633ba8fb2e10b5af61cfe6a9ab2969151d435a
Manmeet Singh <meetsingh459@gmail.com>
2026-09-14 17:21:19 +0530

    Initial Commit

---
+++ NetworkDebuggingTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkDebuggingTool
+//
+//  Created by Manmeet on 14/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkDebuggingTool/NetworkDebuggingToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDebuggingToolApp.swift
+//  NetworkDebuggingTool
+//
+//  Created by Manmeet on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkDebuggingToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
